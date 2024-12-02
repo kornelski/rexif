3f1a8cfa67cc76656b8adb174e42ebeec2714e88
Kornel <kornel@geekhood.net>
2024-12-02 13:37:09 +0000

    Optimize Vec allocation

--- src/lowlevel.rs
+++ src/lowlevel.rs
@@ -73,47 +73,62 @@ pub(crate) fn read_irational(le: bool, raw: &[u8]) -> Option<IRational> {
     Some(IRational { numerator: n, denominator: d })
 }
 
+#[inline(always)]
+fn read_elements<T>(size: u8, count: u32, raw: &[u8], convert: impl Fn(&[u8]) -> T) -> Option<Vec<T>> {
+    let count = count as usize;
+    let size = size as usize;
+    let byte_size = size.checked_mul(count)?;
+    let bytes = raw.get(..byte_size)?;
+
+    let mut out = Vec::new();
+    out.try_reserve_exact(count).ok()?;
+    out.extend(bytes.chunks_exact(size).map(convert).take(count));
+
+    Some(out)
+}
+
 /// Read array from a stream of bytes. Caller must be sure of count and buffer size
 pub(crate) fn read_i8_array(count: u32, raw: &[u8]) -> Option<Vec<i8>> {
     Some(raw.get(..count as usize)?.iter().map(|&i| i as i8).collect())
 }
 
+#[inline(never)]
 /// Read array from a stream of bytes. Caller must be sure of count and buffer size
 pub(crate) fn read_u16_array(le: bool, count: u32, raw: &[u8]) -> Option<Vec<u16>> {
-    Some(raw.get(..count as usize * 2)?.chunks_exact(2).take(count as usize).map(|ch| read_u16(le, ch).unwrap()).collect())
+    read_elements(2, count, raw, move |ch| read_u16(le, ch).unwrap())
 }
 
 /// Read array from a stream of bytes. Caller must be sure of count and buffer size
 pub(crate) fn read_i16_array(le: bool, count: u32, raw: &[u8]) -> Option<Vec<i16>> {
-    Some(raw.get(..count as usize * 2)?.chunks_exact(2).take(count as usize).map(|ch| read_i16(le, ch).unwrap()).collect())
+    read_elements(2, count, raw, move |ch| read_i16(le, ch).unwrap())
 }
 
 /// Read array from a stream of bytes. Caller must be sure of count and buffer size
 pub(crate) fn read_u32_array(le: bool, count: u32, raw: &[u8]) -> Option<Vec<u32>> {
-    Some(raw.get(..count as usize * 4)?.chunks_exact(4).take(count as usize).map(|ch| read_u32(le, ch).unwrap()).collect())
+    read_elements(4, count, raw, move |ch| read_u32(le, ch).unwrap())
 }
 
 /// Read array from a stream of bytes. Caller must be sure of count and buffer size
 pub(crate) fn read_i32_array(le: bool, count: u32, raw: &[u8]) -> Option<Vec<i32>> {
-    Some(raw.get(..count as usize * 4)?.chunks_exact(4).take(count as usize).map(|ch| read_i32(le, ch).unwrap()).collect())
+    read_elements(4, count, raw, move |ch| read_i32(le, ch).unwrap())
 }
 
 /// Read array from a stream of bytes. Caller must be sure of count and buffer size
 pub(crate) fn read_f32_array(count: u32, raw: &[u8]) -> Option<Vec<f32>> {
-    Some(raw.get(..count as usize * 4)?.chunks_exact(4).take(count as usize).map(|ch| read_f32(ch).unwrap()).collect())
+    read_elements(4, count, raw, move |ch| read_f32(ch).unwrap())
 }
 
 /// Read array from a stream of bytes. Caller must be sure of count and buffer size
 pub(crate) fn read_f64_array(count: u32, raw: &[u8]) -> Option<Vec<f64>> {
-    Some(raw.get(..count as usize * 8)?.chunks_exact(8).take(count as usize).map(|ch| read_f64(ch).unwrap()).collect())
+    read_elements(8, count, raw, move |ch| read_f64(ch).unwrap())
 }
 
 /// Read array from a stream of bytes. Caller must be sure of count and buffer size
 pub(crate) fn read_urational_array(le: bool, count: u32, raw: &[u8]) -> Option<Vec<URational>> {
-    Some(raw.get(..count as usize * 8)?.chunks_exact(8).take(count as usize).map(|ch| read_urational(le, ch).unwrap()).collect())
+    read_elements(8, count, raw, move |ch| read_urational(le, ch).unwrap())
 }
 
 /// Read array from a stream of bytes. Caller must be sure of count and buffer size
 pub(crate) fn read_irational_array(le: bool, count: u32, raw: &[u8]) -> Option<Vec<IRational>> {
-    Some(raw.get(..count as usize * 8)?.chunks_exact(8).take(count as usize).map(|ch| read_irational(le, ch).unwrap()).collect())
+    read_elements(8, count, raw, move |ch| read_irational(le, ch).unwrap())
 }
